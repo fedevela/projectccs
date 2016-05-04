267c5cda414e76fff7d4c737f66dfdd8ee0fb698
fedevela <fedevela@fedevela-ASUSK45A>
2016-05-04 14:50:15 -0500

    con intento de glyphicons, ESTABLE

--- public_src/index.js
+++ public_src/index.js
@@ -2,78 +2,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Component from './Component';
-
 import io from 'socket.io-client';
 import feathers from 'feathers/client';
 import hooks from 'feathers-hooks';
 import socketio from 'feathers-socketio/client';
 import localstorage from 'feathers-localstorage';
 import authentication from 'feathers-authentication/client';
+
 import PageHeader from 'react-bootstrap/lib/PageHeader';
-import Tabs from 'react-bootstrap/lib/Tabs';
 import Tab from 'react-bootstrap/lib/Tab';
+import Tabs from 'react-bootstrap/lib/Tabs';
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
 import Button from 'react-bootstrap/lib/Button';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
 const socket = io('http://localhost:3030/');
 
-
-
-const app = feathers()
-        .configure(socketio(socket))
-        .configure(hooks())
-        .configure(authentication({storage: window.localStorage}));
+const app = feathers().configure(socketio(socket)).configure(hooks()).configure(authentication({storage: window.localStorage}));
 
 var UserList = React.createClass({
   getInitialState() {
-//    return {text: new Date()};
+    //    return {text: new Date()};
     return {};
-  },
-  logout() {
-    app.logout().then(() => window.location.href = '/login.html');
   },
 
   registrarVenta(ev) {
-    //  	  console.log("registrando venta 1");
     app.service('servicioRegistroVentas').create(this.state);
-    //    	  console.log("registrando venta 2");
-
-//    this.setState({text: new Date()});
-    //    	  console.log("registrando venta 3");
     ev.preventDefault();
-    //    renderComponent();
-    //this.forceUpdate();
-    //  console.log("vamos");
   },
 
   render() {
     const users = this.props.users;
     return <div>
       <header>
-            <h4>
-          <span>{users.length}</span>
-          users
-        </h4>
+        <Button bsStyle="primary" onClick={this.registrarVenta}>
+          Registrar Venta
+        </Button>
       </header>
       <ListGroup componentClass="ul">
-        {users.map(user =>
-          <ListGroupItem key={user._id}>
-          <img src={user.avatar || PLACEHOLDER} className="avatar"/> {user.email} : {user.numeroDeClics} Ventas
-          </ListGroupItem>
-        )}
+        {users.map(user => <ListGroupItem key={user._id}>
+          <img src={user.avatar || PLACEHOLDER} className="avatar"/> {user.email}
+          : {user.numeroDeClics}
+          Ventas
+        </ListGroupItem>)}
       </ListGroup>
-      <footer>
-          <Button
-              bsStyle="primary"
-              onClick={this.registrarVenta}>
-              Registrar Venta
-            </Button>
-      </footer>
     </div>;
   }
 });
 
+const LogoutButton = React.createClass({
+  logout() {
+    app.logout().then(() => window.location.href = '/login.html');
+  },
+    
+    render(){
+        return <Button onClick={this.logout} bsSize="small" bsStyle="warning">
+                <Glyphicon glyph="close-circle" />
+          Cerrar
+        </Button>
+    }
+});
 
 const ChatApp = React.createClass({
   getInitialState() {
@@ -110,7 +99,6 @@ const ChatApp = React.createClass({
       }
     }).then(page => this.setState({registroVentas: page.data.reverse()}));
 
-
     // Listen to newly created registroVentas
     servicioRegistroVentas.on('created', () => userService.find({
       query: {
@@ -120,40 +108,32 @@ const ChatApp = React.createClass({
       }
     }).then(page => this.setState({users: page.data})));
   },
-  
+
   render() {
-    return <div id="userlist">
-    <UserList users={this.state.users}/>
-    <Tabs defaultActiveKey={1} id="uncontrolled-tab-example"></Tabs>
-  </div>
-  
-    {/*
-    <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
-    <ReactBootstrap.Tab eventKey={1} title="Tablero de Posiciones">
-    </ReactBootstrap.Tab>
-    <ReactBootstrap.Tab eventKey={2} title="Tab 2">Tab 2 content</ReactBootstrap.Tab>
-    <ReactBootstrap.Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</ReactBootstrap.Tab>
-  </Tabs>
-  */}
+    return <Tabs defaultActiveKey={1} id='mainTabs'>
+      <Tab eventKey={1} title="Ranking">
+        <UserList users={this.state.users}/>
+      </Tab>
+      <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
+      <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
+    </Tabs>
   }
 });
 
 app.authenticate().then(() => {
   ReactDOM.render(
     <div id="app">
-        <PageHeader>Cardif CIMA<br/><small>Eslogan</small></PageHeader>
-        <ChatApp/>
-  </div>
-  , document.querySelector('#mainAppContainer'));
+    <PageHeader>Cardif CIMA<br/>
+    {/*
+      <small>Eslogan</small><br/>
+      */}
+      <LogoutButton/>
+    </PageHeader>
+    <ChatApp/>
+  </div>, document.querySelector('#mainAppContainer'));
 }).catch(error => {
   if (error.code === 401) {
     window.location.href = '/login.html'
   }
   console.error(error);
 });
-//window.onload = () => {
-//  ReactDOM.render(
-//    <Component />,
-//    document.querySelector('#container')
-//  );
-//};
